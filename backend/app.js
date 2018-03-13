b0ccdefbaf9496206e5d225094abb1b8ec45a525
AlbertoCota93 <is694734@iteso.mx>
2018-03-13 10:51:13 -0600

    Se crea la base de datos cada vez que se inicia el servicio (cambiar linea de host)

--- backend/app.js
+++ backend/app.js
@@ -7,11 +7,33 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var stemmer = require('porter-stemmer').stemmer;
 var async = require('async');
+var http = require('http');
+var nano = require('nano')('http://172.18.0.3:5984');
 
 // Express
 var app = express();
 
 
+function intervalFunct(){
+  nano.db.get('ez-rides',function(err,req){
+    if(err){
+        console.log(err.message);
+        nano.db.create('ez-rides', function(req,res){
+          if(res){
+            //Created
+            console.log("Se creo la base de datos");
+            clearInterval();
+          } else {
+            console.log("no se creo la base de datos");
+            //Failed
+          }
+        });
+                  
+        }
+    })
+}
+
+setInterval(intervalFunct, 1000);
 
 app.use(function(req, res, next) {
     res.setHeader("Cache-Control", "no-cache must-revalidate");
