3452933765f04af128e70a52d35d6db2df0c664b
AlbertoCota93 <35739316+AlbertoCota93@users.noreply.github.com>
2018-03-14 15:14:16 -0600

    Request By ID and database creation

--- backend/app.js
+++ backend/app.js
@@ -7,11 +7,54 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var stemmer = require('porter-stemmer').stemmer;
 var async = require('async');
+var http = require('http');
+var nano = require('nano')('http://172.18.0.3:5984');
 
 // Express
 var app = express();
+var ez = nano.use('ez-rides');
 
 
+var routarr = ['Lopez Mateos', 'Av La Calma', 'Av Guadalupe', 'Av Naciones Unidas', 'La Minerva'];
+var i = 1;
+function intervalFunct(){
+  nano.db.get('ez-rides',function(err,req){
+    if(err){
+        console.log(err.message);
+        nano.db.create('ez-rides', function(req,res){
+          if(res){
+            for(var a =1; a<= routarr.length;a++){
+              ez.insert({_id: String(a), route:routarr[a-1]}, function(err,body){
+                if(!err){
+
+                } else {
+                  console.log(err);
+                }
+              });
+            }
+            //Created
+            clearInterval();
+          } else {
+            console.log("no se creo la base de datos");
+            //Failed
+          }
+        });
+                  
+        } else {
+          for(var a =1; a<=routarr.length;a++){
+            ez.insert({_id: String(a),route:routarr[a]}, function(err,body){
+              if(!err){
+              } else {
+               
+              }
+            });
+          }
+          clearInterval();
+        }
+    })
+}
+
+setInterval(intervalFunct, 1000);
 
 app.use(function(req, res, next) {
     res.setHeader("Cache-Control", "no-cache must-revalidate");
@@ -26,18 +69,35 @@ app.get('/', function(req, res, next) {
 });
 
 app.get('/request', function(req, res) {
-
+  res.send ({ title: 'Request'});
+  /*ez.list({startkey:'1'}, function(err, body) {
+    if (!err) {
+      body.rows.forEach(function(doc) {
+        res.send(doc);
+      });
+    }
+  });*/
 });
-app.get('/request/{id}',function(req,res){
-
+app.get('/request/:id',function(req,res){
+  //res.send ({ title: 'Request by ID'});
+  var id = req.params.id;
+  ez.get(id, function(err,body){
+      if(err){
+        res.send({title:'Error' ,
+         error: err.message});
+      } else {
+          res.send(body); 
+        }
+      
+  });
 });
 
-app.get('/buttoninfo/{id}',function(req,res){
-
+app.get('/buttoninfo/:id',function(req,res){
+  res.send ({ title: 'Button Info'});
 });
 
 app.get('/active',function(req,res){
-
+  res.send ({ title: 'Active Destinations'});
 });
 
 module.exports = app;
