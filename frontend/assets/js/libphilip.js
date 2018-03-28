26911ef38c7d1ce4f0e158fa37ce4b74b41ccc3a
AlbertoCota93 <is694734@iteso.mx>
2018-03-28 13:24:04 -0600

    whatev

--- frontend/assets/js/libphilip.js
+++ frontend/assets/js/libphilip.js
@@ -1,19 +1,19 @@
 if (!String.format) {
-        String.format = function (format) {
-            var args = Array.prototype.slice.call (arguments, 1);
-            return format.replace (/{(\d+)}/g, function (match, number) {
-            return typeof args[number] != 'undefined'
-              ? args[number]
-          : match;
-            });
-        };
-    }
-    
-    function makeIdEditable (id) {
-        get (id).contentEditable = "true";
-    }
-    
-    function get (id) {
-        // console.log (String.format ("Getting: {0}", id));
-        return document.getElementById(id);
-    } 
+    String.format = function (format) {
+        var args = Array.prototype.slice.call (arguments, 1);
+        return format.replace (/{(\d+)}/g, function (match, number) {
+        return typeof args[number] != 'undefined'
+            ? args[number]
+        : match;
+        });
+    };
+}
+
+function makeIdEditable (id) {
+    get (id).contentEditable = "true";
+}
+
+function get (id) {
+    // console.log (String.format ("Getting: {0}", id));
+    return document.getElementById(id);
+} 
